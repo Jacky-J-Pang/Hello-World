624604e32c016a8e75097a0a4701a01ca8488f35
Jacky250113 <89609924+Jacky250113@users.noreply.github.com>
2021-09-22 05:47:03 -1000

    Create Ex1.js

---
+++ Lab6/Ex1.js
@@ -0,0 +1,11 @@
+var age =  20;
+
+var fav_num = 13;
+
+var day_of_birth = 21;
+
+var month_of_birth = 1;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
