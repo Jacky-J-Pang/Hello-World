7f18cf1bbf14cbff346c795706b7efc5bf95933f
Jacky250113 <89609924+Jacky250113@users.noreply.github.com>
2021-12-14 13:19:06 -1000

    Assignment 3

--- Assignment 3 New/Server.js
+++ Assignment 3 New/Server.js
@@ -16,35 +16,62 @@ var wallpaperemp = {}; //Defines wallpaperemp as a variable that requests the qu
 
 
 // End of variable groups
+// Routing 
 
-app.use(myParser.urlencoded({ extended: true }));
+// monitor all requests
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path);
+   next();
+});
+
+// process purchase request (validate quantities, check quantity available)
+
+// route all other GET requests to files in public 
+app.use(express.static(__dirname + '/public'));
 //GET purchase submission form, if good give an invoice, otherwise send back to order page
-
 app.get("/process_page", function (request, response) {
-    //look up request.query
-    wallpaperemp = request.query;
-    params = request.query;
-    console.log(params);
-    if (typeof params['purchase_submit'] != 'undefined') {  //check if quantity data is valid
-       has_errors = false; // assume quantities are valid from the start
-       total_qty = 0; // need to check if something was selected so we will look if the total > 0
-       for (i = 0; i < products.length; i++) {
-          if (typeof params[`quantity${i}`] != 'undefined') {
-             a_qty = params[`quantity${i}`]; //makes textboxes sticky in case of invalid data change the original code from flag it just makes more sense for me
-             total_qty += a_qty; //Adds up all quantities to check if any quantity is inputed at all
-             if (!isNonNegInt(a_qty)) {
-                has_errors = true; // Invalid quantity
-             }
-          }
-       }
-         // Now respond to errors or redirect to invoice if all is ok
-       qstr = querystring.stringify(request.query);
-       //if quantity data is not valid, send them back to product display
-       if (has_errors || total_qty == 0) { 
-          qstr = querystring.stringify(request.query);
-          response.redirect("products_display.html?" + qstr);
-       } else { // if quantity data is valid, send them to the invoice
-          response.redirect("login.html?" + qstr);
-       }
-    }
- });
+   //look up request.query
+   wallpaperemp = request.query;
+   params = request.query;
+   console.log(params);
+   if (typeof params['purchase_submit'] != 'undefined') {  //check if quantity data is valid
+      has_errors = false; // assume quantities are valid from the start
+      total_qty = 0; // need to check if something was selected so we will look if the total > 0
+      for (i = 0; i < products.length; i++) {
+         if (typeof params[`quantity${i}`] != 'undefined') {
+            a_qty = params[`quantity${i}`]; //makes textboxes sticky in case of invalid data change the original code from flag it just makes more sense for me
+            total_qty += a_qty; //Adds up all quantities to check if any quantity is inputed at all
+            if (!isNonNegInt(a_qty)) {
+               has_errors = true; // Invalid quantity
+            }
+         }
+      }
+           // Now respond to errors or redirect to invoice if all is ok
+           qstr = querystring.stringify(request.query);
+           //if quantity data is not valid, send them back to product display
+           if (has_errors || total_qty == 0) { 
+              qstr = querystring.stringify(request.query);
+              response.redirect("products_display.html?" + qstr);
+           } else { // if quantity data is valid, send them to the invoice
+              response.redirect("login.html?" + qstr);
+           }
+        }
+     });
+     //Ensures data inputted isn not a negative number, does not contain letters and is not a decimal
+function isNonNegInt(q, returnErrors = false) {
+   errors = []; // assume no errors at first
+   if (q == "") { q = 0; } //handle blank inputs as if they are 0
+   if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+   if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+   if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+   //if (params[`quantity${i}`] <= products[i].quantity_available) errors.push ('Quantity exceeds in current stocks') NEED TO CHECK THE FUNCTION KEEPS CRASHING
+   // CANT UPDATE INVENTORY EITHER 
+   return returnErrors ? errors : (errors.length == 0); //returns as error
+}
+
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path); //respond to HTTP request by sending type of request and the path of request
+   next(); //calls the middleware function
+});
+// start server
+app.listen(8080, () => console.log(`listening on port 8080`));
--- Jacky_Pang Assignment2/public/product.js
+++ Jacky_Pang Assignment2/public/product.js
@@ -1,51 +1,51 @@
 //Description: Array that contains all five products with price, descr, image,. and quantity available.
 //Jacky Pang 11/30/2021
 //Alright Gotta remake the entire website Shit keep givin me module missing or overflow erros TF? - Jacky 
-var products = [
+products = [
     {
-        "name": "Fate",
-        "price": 9.99,
-        "description": "Fate Unlimited Blade Works",
-        "image": "./images/Fate.png",
+      "name": "Pantheon The Aspect of War",
+      "price": 9.99,
+      "description": "A spartan warrior with a vengance to the gods",
+      "image": "./images/Fate.png",
+      "quantity_available": 6
+      },
+      {
+      "name": "Tryndamere: The Barbarian King",
+      "price": 9.99,
+      "description": "Angry man that is to angry to die",
+      "image": "./images/Gojo.jpg",
+      "quantity_available": 6
+      },
+      {
+      "name": "Braum The Frejlord Strongman",
+      "price": 12.99,
+      "description": "Shielder for the weak and mustache for the righteous",
+      "image": "./images/Lance.png",
+      "quantity_available": 6
+      },
+      {
+      "name": "Caityln : The Piltover Sherriff",
+      "price": 9.99,
+      "description": "The enforcer and the Piltover sniper",
+      "image": "./images/DemonSlayer.png",
+      "quantity_available": 6
+      },
+    
+      {
+        "name": "Nautlis: The ancient water giant",
+        "price": 8.99,
+        "description": "All will pay the ocean, it's tilthe",
+        "image": "./images/Unicorn.jpg",
         "quantity_available": 6
-        },
-        {
-        "name": "Gojo",
-        "price": 9.99,
-        "description": "From Jujutsu Kaisem Mappa animation",
-        "image": "./images/Gojo.jpg",
+      },
+      {
+        "name": "Sion: The Undead Juggernaut ",
+        "price": 7.99,
+        "description": "Denied Oblivion, he was resurrected to serve his empire even in depth",
+        "image": "./images/GreenDude.png",
         "quantity_available": 6
-        },
-        {
-        "name": "Lance",
-        "price": 12.99,
-        "description": "Brave Frontier from Grand Gaia",
-        "image": "./images/Lance.png",
-        "quantity_available": 6
-        },
-        {
-        "name": "Demon Slayer Fan Art",
-        "price": 9.99,
-        "description": "Tanjiro from demon slaer.",
-        "image": "./images/DemonSlayer.png",
-        "quantity_available": 6
-        },
-      
-        {
-          "name": "Unicorn",
-          "price": 8.99,
-          "description": "For all Space, Sci-fi and mech fans",
-          "image": "./images/Unicorn.jpg",
-          "quantity_available": 6
-        },
-        {
-          "name": "Susanoo",
-          "price": 7.99,
-          "description": "God of destruction from Blazblue",
-          "image": "./images/GreenDude.png",
-          "quantity_available": 6
-          }
- ];
+        }
+];
  
  if(typeof module != 'undefined') {
      module.exports = products;
